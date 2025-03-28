75cf39b4462980971ed0f3440f95c7d6b60fbb4e
TomasCarrizoAslanian <tcarrizoaslanian@mail.austral.edu.ar>
2025-03-28 12:48:34 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,19 +1,19 @@
 def change():
     gasto = 23.75
-dinero_recibido = 100
+    dinero_recibido = 100
 
-vuelto_total = dinero_recibido - gasto
-pesos = int(dinero_recibido - gasto)
-centavos = int(round((vuelto_total - pesos) * 100))
+    vuelto_total = dinero_recibido - gasto
+    pesos = int(vuelto_total)
+    centavos = int(round((vuelto_total - pesos) * 100))
 
-print("Ingresar gasto:")
-print(gasto)
-print("Dinero Recibido:")
-print(dinero_recibido)
-print()
-print("Vuelto")
-print()
-print("Pesos:")
-print(pesos)
-print("Centavos:")
-print(centavos)
+    print("Ingresar gasto:")
+    print(gasto)
+    print("Dinero recibido")
+    print(dinero_recibido)
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
