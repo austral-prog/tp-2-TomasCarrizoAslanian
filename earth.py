f6d92b24c1eacab63c4e6c637e61482a92721963
TomasCarrizoAslanian <tcarrizoaslanian@mail.austral.edu.ar>
2025-03-28 12:43:56 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
-    y = "Barbados"
+y = "Barbados"
+
+resultado1 = x < y
+resultado2 = y < x
+
+print("The result of X comes first in the dictionary than Y is " + str(resultado1) + ".")
+print("The result of X comes first in the dictionary than Y is " + str(resultado2) + ".")
